a5be3ca8649f38bce56c4e7b192098a47e031b0e
derinov <derinov@derinov-PC.(none)>
2012-02-28 10:55:35 +0400

    hello_desktop

--- zalivator/source/test.py
+++ zalivator/source/test.py
@@ -1,6 +1,30 @@
 import sys
-from PyQt4.QtGui import *
-app = QApplication(sys.argv)
-button = QPushButton("Hello World", None)
+from PyQt4 import QtCore, QtGui
+
+app = QtGui.QApplication(sys.argv)
+
+MainWindow = QtGui.QMainWindow()
+menubar = QtGui.QMenuBar(MainWindow)
+menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+menubar.setObjectName("menubar")
+
+menuFile = QtGui.QMenu(menubar)
+menuFile.setObjectName("menuFile")
+MainWindow.setMenuBar(menubar)
+
+actionupdateall = QtGui.QAction(MainWindow)
+actionupdateall.setObjectName("actionupdateall")
+actionupdateall.setText("Whee")
+
+menuFile.addSeparator()
+menuFile.addAction(actionupdateall)
+menubar.addAction(menuFile.menuAction())
+
+menuFile.setTitle(QtGui.QApplication.translate("MainWindow", "File", None, QtGui.QApplication.UnicodeUTF8))
+
+button = QtGui.QPushButton("Hello World", MainWindow)
 button.show()
-app.exec_()
+
+MainWindow.show()
+
+sys.exit(app.exec_())
