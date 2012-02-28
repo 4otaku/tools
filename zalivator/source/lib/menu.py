4723030077329c53b0b729232d5846283ef40002
derinov <derinov@derinov-PC.(none)>
2012-02-28 17:34:15 +0400

    zalivator_actions_help

--- zalivator/source/lib/menu.py
+++ zalivator/source/lib/menu.py
@@ -22,14 +22,24 @@ class Menu(Abstract):
 
             menu_upload = QtGui.QMenu(menubar)
             menu_upload.setObjectName("menu_upload")
-            window.setMenuBar(menubar)
 
             self.actions['art'] = art.Action_Art(app, menu_upload)
             self.actions['cg_pack'] = cg_pack.Action_Pack(app, menu_upload)
             self.actions['post'] = post.Action_Post(app, menu_upload)
             self.actions['quit'] = quit.Action_Quit(app, menu_upload)
-            menu_upload.setTitle(self.get_app().utf('Залить'))
 
+            menu_upload.setTitle(self.utf('Залить'))
             menubar.addAction(menu_upload.menuAction())
+
+            menu_about = QtGui.QMenu(menubar)
+            menu_about.setObjectName("menu_upload")
+
+            self.actions['about'] = about.Action_About(app, menu_about)
+            self.actions['help'] = help.Action_Help(app, menu_about)
+
+            menu_about.setTitle(self.utf('Помощь'))
+            menubar.addAction(menu_about.menuAction())
+
+            window.setMenuBar(menubar)
         except Exception as E:
             Error(E).display()
