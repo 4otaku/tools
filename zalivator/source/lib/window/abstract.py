b067d95610b7a427a1faf4e4c69becfaa3849962
Nameless <admin@4otaku.ru>
2012-03-05 23:04:22 +0400

    workan

--- zalivator/source/lib/window/abstract.py
+++ zalivator/source/lib/window/abstract.py
@@ -15,11 +15,18 @@ class Window_Abstract(Abstract):
             if widget:
                 widget.close()
 
+            self._layout = QtGui.QVBoxLayout()
+
             self._box = QtGui.QGroupBox(self.get_window())
             self._box.setGeometry(0, 0, 600, 480)
+            self._box.setLayout(self._layout)
+
             self.get_window().setCentralWidget(self._box)
         except Exception as E:
             Error(E).display()
 
     def get_box(self):
         return self._box
+
+    def get_layout(self):
+        return self._layout
--- zalivator/source/lib/window/pack.py
+++ zalivator/source/lib/window/pack.py
@@ -6,16 +6,28 @@ from PyQt4 import QtCore, QtGui
 # ======================================================================
 class Window_Pack(Window_Abstract):
 
-    stateIndex = 2
-
     def __init__(self, app):
         Window_Abstract.__init__(self, app)
 
         self._button = QtGui.QPushButton(self.utf("Выберите файл"), self.get_box())
-        self._button.show()
+        self._button.clicked.connect(self.select_file)
+        self.get_layout().addWidget(self._button)
 
-#        self.fileDialog = QtGui.QFileDialog(self.get_window())
-#        self.fileDialog.show()
+        self._title = QtGui.QLineEdit(QtCore.QString(self.utf('Введите заголовок')), self.get_box())
+        self.get_layout().addWidget(self._title)
 
-    def select_file():
-        lineEdit.setText(QFileDialog.getOpenFileName())
+        self._text = QtGui.QTextEdit(QtCore.QString(self.utf('Введите описание')), self.get_box())
+        self.get_layout().addWidget(self._text)
+
+        self._send = QtGui.QPushButton(self.utf("Отправить"), self.get_box())
+        self._send.clicked.connect(self.send_file)
+        self.get_layout().addWidget(self._send)
+
+    def select_file(self, data):
+        self._file_dialog = QtGui.QFileDialog(self.get_window())
+        self._file_dialog.show()
+#        lineEdit.setText(QFileDialog.getOpenFileName())
+
+    def send_file(self, data):
+        self._file_dialog = QtGui.QFileDialog(self.get_window())
+        self._file_dialog.show()
