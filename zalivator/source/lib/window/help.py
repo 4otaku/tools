b399ab546f548229d36f04cd4178e34fe9a69def
Nameless <admin@4otaku.ru>
2012-03-10 20:38:37 +0400

    help

--- zalivator/source/lib/window/help.py
+++ zalivator/source/lib/window/help.py
@@ -2,12 +2,31 @@
 # License avaiavle at zalivator_license.txt in "licenses" directory
 
 from lib.window.text import Window_Text
+from PyQt4 import QtCore, QtGui
+import os, sys
 
 # ======================================================================
 class Window_Help(Window_Text):
 
-    text = 'Добро пожаловать в заливтор чотаку. <br />' + \
-        'Для начала работы выберите один из пунктов в меню "Залить".'
+    text = 'Последняя версия справки доступна по адресу: ' + \
+        '<a href="http://wiki.4otaku.ru/Zalivator">http://wiki.4otaku.ru/Zalivator</a>.'
 
     def __init__(self, app):
         Window_Text.__init__(self, app)
+
+        self._help = QtGui.QTextBrowser(self.get_box())
+
+        path = os.path.abspath(os.path.dirname(sys.argv[0]))
+
+        f = open(path + os.sep + 'help.txt', 'r')
+        data = f.read()
+        f.close()
+
+        self._help.setText(data)
+
+        self._help.setOpenExternalLinks(True)
+        self._help.setFrameStyle(QtGui.QFrame.NoFrame)
+        self._help.setWordWrapMode(QtGui.QTextOption.WrapAtWordBoundaryOrAnywhere)
+        self._help.setReadOnly(True)
+
+        self.get_layout().addWidget(self._help)
